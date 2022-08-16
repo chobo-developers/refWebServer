973bbe86fadebf222c061b9ced6f6b97ca15c833
bjk1649 <qhwns1649@naver.com>
2022-08-16 08:09:26 +0000

    feat : fcm 서비스 모듈 추가

---
+++ fcmService.js
@@ -0,0 +1,25 @@
+import serviceAccount from './firebaseKey.json' assert {type : "json"};
+import { admin } from 'firebase-admin/app';
+
+admin.initializeApp({
+    credential : admin.credential.cert(serviceAccount)
+});
+
+const registrationToken = '';
+
+const message = {
+    data :{
+        title: '제목',
+        body: '내용',
+    },
+    toekn : registrationToken
+};
+
+admin.messaging().send(message).
+    then((response)=>
+    {
+        consolg.log('successfully sent message: ',response) ;
+    })
+    .catch((error)=>{
+        console.log('error sending message: ',error);
+    })
