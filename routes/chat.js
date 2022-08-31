caf6e024b2b9b8c6d10fa1f61e82e0586af93ab5
bjk1649 <qhwns1649@naver.com>
2022-08-31 09:29:08 +0000

    fix : 메시지 전송 api에서 토큰 가져오는 방식 수정(유저 아이디 비교)

--- routes/chat.js
+++ routes/chat.js
@@ -7,16 +7,28 @@ const router = express.Router();
 
 //메시지 보내기
 router.post('/sendMessage',async (req,res)=>{
-    const chatId = req.body.chatId;
-    const message = req.body.message;
-    const title = "보낸사람: " + req.body.fromId;
-    const toId = req.body.toID;
-    const createdAt = req.body.createdAt;
-    const params = [chatId,createdAt,message,req.body.fromId];
+    const chatId = req.body.chat_Id;
+    const message = req.body.content;
+    const from = req.body.from;
+    const createdAt = req.body.created_at;
+
+    const title = "보낸사람: " + from;
+    const toId = from;
+    const params = [chatId,createdAt,message,req.body.from];
+
+    //join 으로 채팅에 참여한 사람들 아이디 2개 가져온 후, 메시지를 보낸 사람과 비교해 메시지를 보낸 사람이 아닌 사람(즉, 메시지를 받는 사람)에게 알림 전송
+    let sql = 'select P.user_id as writer_id, C.user_id as contact_id from mainDB.chat C join mainDB.post P on C.post_id =P.id where C.id = ?';
+    let response = await requestDB(sql,chatId);
+    if (response.result[0].writed_id == from){
+        toId = response.result[0].contact_id;
+    }
+    else {
+        toId = response.result[0].writer_id;
+    }
 
     //fcm에 정보 전달
-    let sql = 'select fcm_token from user where id = ?';
-    let response = await requestDB(sql,toId);
+    sql = 'select fcm_token from user where id = ?';
+    response = await requestDB(sql,toId);
     const token = response.result[0].fcm_token;
     requestFcm(token, title, message);
 
@@ -33,6 +45,7 @@ router.post('/makeChat',async (req,res)=>{
     const id = req.body.id;
     const chatId = String(postId) + contactId;
     const createdAt = req.body.createdAt;
+
 
     let sql = 'select from chat where id = ?';
     let response = await requestDB(sql,chatId);
