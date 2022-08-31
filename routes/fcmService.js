d902bc809362d81a6a4c953cc306301e96b7e73b
bjk1649 <qhwns1649@naver.com>
2022-08-31 10:29:50 +0000

    fix: fcm service fcm-node 사용확정,message는 data없이 일단 notification만 사용하도록  주석처리

--- routes/fcmService.js
+++ routes/fcmService.js
@@ -1,12 +1,14 @@
 
-import serviceAccount from './firebaseKey.json' assert {type : "json"};
-import { admin } from 'firebase-admin/app';
+// import serviceAccount from './firebaseKey.json' assert {type : "json"};
+// import { admin } from 'firebase-admin/app';
 
 //firebase-admin 과 차이 알아보고 수정예정
 const FCM = require('fcm-node');
 const serverKey = ''; //서버키
 const fcm = new FCM(serverKey);
-
+// admin.initializeApp({
+//     credential : admin.credential.cer(serviceAccount)
+// });
 export const requestFcm = function (token,title,content){
     
     const message = {
@@ -15,12 +17,12 @@ export const requestFcm = function (token,title,content){
         notification: {
             title: title, //알림 제목
             body: content //알림 내용
-        },
-        
-        data: {  // 전송할 데이터가 추가로 있는 경우
-            my_key: 'my value',
-            my_another_key: 'my another value'
         }
+        // ,
+        // data: {  // 전송할 데이터가 추가로 있는 경우
+        //     my_key: 'my value',
+        //     my_another_key: 'my another value'
+        // }
     };
     //오류처리
     fcm.send(message, function(err, response){
