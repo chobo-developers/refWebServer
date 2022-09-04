2dc0746f3e690b2486f713a89da72439c06b2cbe
bjk1649 <qhwns1649@naver.com>
2022-09-04 05:00:20 +0000

    feat: 메일 전송 모듈 추가

---
+++ routes/mailService.js
@@ -0,0 +1,35 @@
+import nodemailer from 'nodemailer';
+import info from './info.json' assert {type : "json"};
+
+export const sendEmail = async (id, content) => {
+    const transporter = nodemailer.createTransport({
+        service: `gmail`,
+        port: 465, //smtp 포트 이용
+        secure: true, // true for 465, false for other ports
+        auth: { 
+            user: info.gmail,
+            pass: info.password,
+        },
+    });
+    const emailOptions = { // 옵션값 설정
+        from: info.gmail,
+        to: info.gmail,
+        subject: `신고 접수`,
+        html: `content`,
+    };
+
+    // 전송
+    // 고려사항 : promise 객체 사용해서 async 빼고 then, catch 로 사용할 수 있지 않을까?
+    return new Promise((resolve, reject) => {
+        transporter.sendMail(emailOptions, (err, res) => {
+            if (err) {
+                console.log(`failed... => `, err);
+                reject(err);
+            } else {
+                console.log(`succeed... => `, res);
+                resolve(res);
+            }
+            transporter.close();
+        });
+});
+}
