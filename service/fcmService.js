b9132f303f13f90cf3dcd30d1ddc58ddcaef75f8
bjk1649 <qhwns1649@naver.com>
2022-09-08 05:16:00 +0000

    fix: 주석정리

--- service/fcmService.js
+++ service/fcmService.js
@@ -1,52 +1,12 @@
 
-// import serviceAccount from './firebaseKey.json' assert {type : "json"};
-// import { admin } from 'firebase-admin/app';
-
-//firebase-admin 과 차이 알아보고 수정예정
-// const FCM = require('fcm-node');
-
-// admin.initializeApp({
-//     credential : admin.credential.cer(serviceAccount)
-// });
-
-// import { FCM } from 'fcm-node';
-
-
-
-// import pkg from 'fcm-node';
-
-// const { FCM } = pkg;
-// const serverKey = 'fyghj'; //서버키
-// const fcm = new FCM(serverKey);
-// export const requestFcm = function (token,title,content){
-    
-//     const message = {
-//         to: token,  //db에서 가져온 토큰
-        
-//         notification: {
-//             title: title, //알림 제목
-//             body: content //알림 내용
-//         }
-//         // ,
-//         // data: {  // 전송할 데이터가 추가로 있는 경우
-//         //     my_key: 'my value',
-//         //     my_another_key: 'my another value'
-//         // }
-//     };
-//     //오류처리
-//     fcm.send(message, function(err, response){
-//         if (err) {
-//             console.log("Something has gone wrong!");
-//         } else {
-//             console.log("Successfully sent with response: ", response);
-//         }
-//     });
-// }
+//fcm service 구현중
 
 import admin from 'firebase-admin';
 import serviceAccount from '../service-account-file.json' assert {type : "json"};
 import express from 'express';
 const router = express.Router();
+
+
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
@@ -60,26 +20,24 @@ router.get('/test', (req, res, next) => {
     let body = req.body.body;
 
     let target_token =
-        '토큰 값..';
+        req.body.token
 
     let message = {
         data: {
-        title: 'test title',
-        body : 'test body',
+        title: title,
+        body : body,
         },
         token: target_token,
     }
-
-    admin
-        .messaging()
-        .send(message)
-        .then(function (response) {
-        res.status(200).send("push success");
-        console.log('Successfully sent message: : ', response)
-        })
-        .catch(function (err) {
-        console.log('Error Sending message!!! : ', err)
-        })
+    admin.messaging().send(message)
+    .then((response) => {
+        console.log('Successfully sent message:', response);
+        res.status(200).send('success');
+    })
+    .catch((error) => {
+        console.log('Error sending message:', error);
+        res.status(500).send('error');
+    });
     });
 
 export default router;
